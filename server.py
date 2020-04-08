f501ad0aa5af2ad33427266edff2b2de4f1ce44c
Yannick Gibson <yannick.gibson@seznam.cz>
2020-04-08 11:07:36 +0200

    adding favicon into static folder, and routing there in server.py

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 from flask_socketio import SocketIO, emit
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "57dwad86a465d79"
-socketio = SocketIO(app, cors_allowed_origins='*')
+socketio = SocketIO(app)
 
 
 @socketio.on('message')
@@ -25,6 +26,11 @@ def disconnected():
 @app.route('/')
 def index():
     return  render_template('index.html')
+    
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
