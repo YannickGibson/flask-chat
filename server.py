ea9dc2a70283d600f977d44ab69da3725ba31629
Yannick Gibson <gibsoyan@fit.cvut.cz>
2025-01-24 11:05:41 +0100

    Fix commit

---
+++ server.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, render_template, send_from_directory
+from flask_socketio import SocketIO, emit
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = ""
+socketio = SocketIO(app)
+
+
+@socketio.on('message')
+def handleMessage(msg):
+    print('\nMessage: {}\n'.format(msg))
+    emit('message', msg, broadcast=True)
+
+###
+
+@socketio.on('connect')
+def connected():
+    print("\nClient connected")
+
+@socketio.on('disconnect')
+def disconnected():
+    print('\nClient disconnected')
+
+
+@app.route('/')
+def index():
+    return  render_template('index.html')
+    
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+    
---
+++ static/js/script.js
@@ -0,0 +1,20 @@
+var socket = io({transports: ['websocket']});
+$(document).ready(()=>{
+    socket.on('message', (msg)=> {
+        $("#message-ul").append("<li>" + msg + "</li>")
+    });
+
+    $("#send-button").click((target)=>{
+        console.log("clicked");
+        socket.emit(
+            'message', 
+            $('#message-input').val()
+        );
+        $('#message-input').val('') //clear input text
+    });
+    $("#message-input").keypress((e)=>{
+        if (e.which == 13){//enter
+            $("#send-button").click();
+        }
+    });
+ });
